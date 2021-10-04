e52f533a79f2031c83a0662d9b2522b9e3eaf614
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-04 09:52:07 -0300

    Adicionado Classes Person e Phone (entidades)

---
+++ src/main/java/com/digitalinnovationone/personapi/entities/Person.java
@@ -0,0 +1,18 @@
+package com.digitalinnovationone.personapi.entities;
+
+import java.util.List;
+
+public class Person {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String cpf;
+
+    private String birthDate;
+
+    private List<Phone> phones;
+}
---
+++ src/main/java/com/digitalinnovationone/personapi/entities/Phone.java
@@ -0,0 +1,12 @@
+package com.digitalinnovationone.personapi.entities;
+
+import com.digitalinnovationone.personapi.enums.PhoneType;
+
+public class Phone {
+
+    private Long id;
+
+    private PhoneType type;
+
+    private String number;
+}
