6301ff70b05878a390b6b28ac6e7ecc6a7cbc001
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-04 09:47:13 -0300

    Adicionado Classes base

---
+++ src/main/java/com/digitalinnovationone/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,20 @@
+package com.digitalinnovationone.personapi.dto.request;
+
+import com.digitalinnovationone.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneDTO {
+
+    private Long id;
+
+    private PhoneType type;
+
+    private String number;
+}
