85f80a170729fada3df40cea92e18a294bd92339
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-04 11:11:20 -0300

    Adicionado Operação Criar person

---
+++ src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.digitalinnovationone.personapi.service;
+
+import com.digitalinnovationone.personapi.dto.response.MessageResponseDTO;
+import com.digitalinnovationone.personapi.entities.Person;
+import com.digitalinnovationone.personapi.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created Person with ID " + savedPerson.getId())
+                .build();
+    }
+}
