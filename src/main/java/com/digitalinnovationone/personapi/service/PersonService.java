0fc10ee5c460d80d342157983a533c07b939db18
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-05 16:02:24 -0300

    Adicionado Método Delete Person por ID

--- src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -47,4 +46,15 @@ public class PersonService {
                 .orElseThrow(() -> new PersonNotFoundException(id));
         return personMapper.toDTO(person);
     }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
 }
